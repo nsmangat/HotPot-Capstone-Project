026f73f078bc43c5d98db9ef290883368fdf9035
eazaz <eazaz@WINDELL-8GE8QTQ>
2024-06-06 17:15:18 -0400

    Removed an import.

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -3,7 +3,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
