0fe332a5b6bebeb45d707bdc298bdcbc12be8e47
Rachel Le <letra1712@gmail.com>
2024-06-10 21:54:38 -0400

    update android app login

--- Frontend/firebase/firebase.js
+++ Frontend/firebase/firebase.js
@@ -1,14 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, initializeAuth, getReactNativePersistence} from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { Platform } from 'react-native';
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_API_KEY, 
+  apiKey: Platform.OS === 'ios' ? process.env.EXPO_PUBLIC_API_KEY:process.env.EXPO_PUBLIC_API_KEY_ANDROID, 
   authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN, 
   projectId: process.env.EXPO_PUBLIC_PROJECT_ID, 
   storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET, 
   messagingSenderId: process.env.EXPO_PUBLIC_MSG_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_APP_ID, 
+  appId: Platform.OS === 'ios' ? process.env.EXPO_PUBLIC_APP_ID:EXPO_PUBLIC_APP_ID_ANDROID, 
 };
 
 const app = initializeApp(firebaseConfig);
