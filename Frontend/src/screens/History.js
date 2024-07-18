8b984883490f4f5dad5495d1e7a293d20fad0622
Navdeep Mangat <nmangat0337@conestogac.on.ca>
2024-07-18 00:42:56 -0400

    adjust image margins so first 2 images fully visible without scrolling on both device platforms, adjust title component margins

--- Frontend/src/screens/History.js
+++ Frontend/src/screens/History.js
@@ -118,11 +118,6 @@ const History = () => {
       ]}
     >
       <ScreenTitle name="history" title="Reports History" />
-
-      <View style={styles.itemContainer}>
-        <ThemedText style={styles.title}>Location</ThemedText>
-        <ThemedText style={styles.title}>Time Reported</ThemedText>
-      </View>
       <View style={styles.list}>
         <FlatList
           data={history}
