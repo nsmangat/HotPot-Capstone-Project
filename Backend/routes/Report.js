b2b25ae0623ed4aa48db9eef3b0ad7f37a717dc5
Navdeep Mangat <nmangat0337@conestogac.on.ca>
2024-07-30 16:00:24 -0400

    Get rid of comment block of old code

--- Backend/routes/Report.js
+++ Backend/routes/Report.js
@@ -77,38 +77,6 @@ router.post("/", async (req, res) => {
     console.log(err);
     return res.status(500).send("Error reporting pothole");
   }
-
-  // try {
-  //   const newPothole = await Pothole.create({
-  //     pothole_size: req.body.description,
-  //     description: req.body.details,
-  //     coordinates: req.body.coordinates,
-  //     address: req.body.address,
-  //     number_of_reports: 1,
-  //     is_fixed: false,
-  //     first_reported_date: new Date(),
-  //     updated_at: new Date(),
-  //     is_reported: false,
-  //   });
-
-  //   //if pothole was created successfully, create the report history for it
-  //   if (newPothole) {
-  //     try {
-  //       const newReport = await Report.create({
-  //         pothole_id: newPothole.pothole_id,
-  //         firebase_uid: firebase_uid,
-  //         report_date: newPothole.first_reported_date,
-  //       });
-  //       res.status(201).json([newPothole, newReport]);
-  //     } catch (err) {
-  //       console.log(err);
-  //       res.status(500).send("Error reporting pothole");
-  //     }
-  //   }
-  // } catch (err) {
-  //   console.log(err);
-  //   res.status(500).send("Error reporting pothole");
-  // }
 });
 
 module.exports = router;
